6e128815ca1d808f76ae3a974c8a5fe7ddd3f411
Jason Divock <jdivock@gmail.com>
2015-10-25 15:03:21 -0700

    node 4 goodness

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
 'use strict';
 
-var request = require('request'),
-    cheerio = require('cheerio'),
-    program = require('commander'),
-    async = require('async'),
-    objectAssign = require('object-assign'),
-    debug = require('debug')('StackRip:index.js'),
-    fs = require('fs');
+let Debug = require('debug');
+let async = require('async');
+let cheerio = require('cheerio');
+let fs = require('fs');
+let path = require('path');
+let program = require('commander');
+let request = require('request');
+
+Debug.enable('*');
+
+let debug = Debug('StackRip:index.js');
 
 request = request.defaults({jar: true});
 
-program
-    .version('0.1.0')
-    .option('-d, --destination [location]', 'Download destination')
-    .option('-w, --episode [episdoeNumber]', 'Stack episode')
-    .parse(process.argv);
+program.version('0.1.0')
+  .option('-d, --destination [location]', 'Download destination')
+  .option('-w, --episode [episdoeNumber]', 'Stack episode')
+  .parse(process.argv);
 
-var STACK_URL = 'http://hypem.com/stack/';
-var destination = program.destination || '.';
+let STACK_URL = 'http://hypem.com/stack/';
+let destination = program.destination || '.';
 
-function getSongURLs($page){
-    return JSON.parse($page('#displayList-data').text()).tracks;
+function getSongURLs($page) {
+  return JSON.parse($page('#displayList-data').text()).tracks;
 }
 
-function getSongDownloadLocations(songs){
-    return new Promise(function(resolve, reject){
-        async.map(songs,
-                  function(song, cb){
-                      request.get('http://hypem.com/serve/source/' +  song.id + '/'
-                                  + song.key,
-                                  function(err, resp, body){
-                                      if(err){
-                                          debug.err('Error fetching dl location');
-                                          cb('Error fetching dl location');
-                                      }
+function getSongDownloadLocations(songs) {
+  return new Promise((resolve, reject) => {
+    async.map(
+        songs,
+        (song, cb) => {
+          request.get(
+              `http://hypem.com/serve/source/${song.id}/${song.key}`,
+              (err, resp, body) => {
+                if (err) {
+                  debug.err('Error fetching dl location');
+                  cb('Error fetching dl location');
+                }
 
-                                      //console.log(body);
-                                      cb(null, objectAssign(song, JSON.parse(body)));
-                                  });
-                  },
-                  function(err, results){
-                      if(err){
-                          reject(err);
-                      }
+                cb(null, Object.assign(song, JSON.parse(body)));
+              });
+        },
+        (err, results) => {
+          if (err) {
+            reject(err);
+          }
 
-                      resolve(results);
-                  }
-                 );
-    });
-
+          resolve(results);
+        }
+    );
+  });
 }
 
-
-request(STACK_URL + program.episode, function(err, resp, html){
-    if(err){
+request(
+    `${STACK_URL}${program.episode}`,
+    (err, resp, html) => {
+      if (err) {
         debug.err('Error fetching url');
         return;
-    }
+      }
 
-    getSongDownloadLocations(getSongURLs(cheerio.load(html))).then(function(results){
-        results.forEach(function(song){
-            console.log('downloading ' + song.url);
-            request(song.url).pipe(fs.createWriteStream(destination + '/' + song.artist + ' - ' + song.song + '.mp3'));
+      getSongDownloadLocations(getSongURLs(cheerio.load(html)))
+        .then(results => {
+          results.forEach(song => {
+            debug(`downloading ${song.url}`);
+
+            request(song.url)
+              .pipe(
+                fs.createWriteStream(
+                  path.join(
+                    destination,
+                    `${song.artist}-${song.song}.mp3`
+                  )
+                )
+              );
+          });
         });
     });
-});
