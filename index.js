2ca0119bbe1171657f1decaa358591190a46dba5
Jason Divock <jdivock@gmail.com>
2016-03-19 11:39:34 -0700

    Typo and forcing output all the time

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const debug = require('debug')('StackRip:index.js');
+/* eslint new-cap: 0 */
+const Debug = require('debug');
+const debug = Debug('StackRip:index.js');
 const program = require('commander');
+
+Debug.enable('*');
 
 const stackRip = require('./lib/stackRip');
 
 program.version(require('./package.json').version)
   .option('-d, --destination [location]', 'Download destination')
-  .option('-w, --episode [episdoeNumber]', 'Stack episode')
+  .option('-w, --episode [episodeNumber]', 'Stack episode')
   .parse(process.argv);
 
 debug('firing up stackrip');
